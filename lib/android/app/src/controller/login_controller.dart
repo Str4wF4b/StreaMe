dd3a9708567ab981ae36242230585d3875e5b4e7
Fabio <fabioalta25@gmail.com>
2022-06-14 00:42:13 +0200

    first little program

--- lib/android/app/src/controller/login_controller.dart
+++ lib/android/app/src/controller/login_controller.dart
@@ -1,3 +1,24 @@
-class LoginController {
+import 'package:mvc_pattern/mvc_pattern.dart';
+import 'package:stream_me/android/app/src/model/login_model.dart';
 
+
+class LoginController extends ControllerMVC {
+  factory LoginController() {
+    if (_this == null) _this = LoginController._();
+    return _this;
+  }
+
+  static LoginController _this = LoginController();
+  
+  LoginController._();
+  
+  int get counter => LoginModel.counter;
+
+  void incrementCounter() {
+    LoginModel.incrementCounter();
+  }
+
+  void decrementCounter() {
+    LoginModel.decrementCounter();
+  }
 }
