656a4f6e88549405ef86ea242bee8b5d42f5488c
Fabio <fabioalta25@gmail.com>
2023-07-03 21:41:01 +0200

    movie and series data to handle stream page

---
+++ lib/android/app/src/model/stream_model.dart
@@ -0,0 +1,29 @@
+class Stream {
+  final int id;
+  final String title;
+  final String type;
+  final String year;
+  final String pg;
+  final String seasonOrDuration;
+  final List<String> genre;
+  final List<String> cast;
+  final List<String> direction;
+  final String plot;
+  final List<String> provider;
+  final String image;
+
+  const Stream({
+    required this.id,
+    required this.title,
+    required this.type,
+    required this.year,
+    required this.pg,
+    required this.seasonOrDuration,
+    required this.genre,
+    required this.cast,
+    required this.direction,
+    required this.plot,
+    required this.provider,
+    required this.image,
+  });
+}
