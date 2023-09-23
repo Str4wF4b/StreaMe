5b4480c04201064bc6b0c72a32b40a6a11ac7ecf
Fabio <fabioalta25@gmail.com>
2023-09-23 15:45:10 +0200

    name change

---
+++ lib/android/app/src/widgets/global/streame_tab.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:stream_me/android/app/src/utils/color_palette.dart';
+
+class StreaMeTab extends StatelessWidget {
+  final String tabTitle;
+  final int tabIndex;
+  final TabController tabController;
+  final bool widthNeeded;
+
+  StreaMeTab(
+      {super.key,
+      required this.tabTitle,
+      required this.tabIndex,
+      required this.tabController,
+      required this.widthNeeded});
+
+  final ColorPalette color = ColorPalette();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 35,
+      width: widthNeeded ? 100 : null,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+          border: Border.all(
+              color: tabController.index == tabIndex
+                  ? color.bodyTextColor
+                  : Colors.grey,
+              width: 1.0)),
+      child: Align(
+        alignment: Alignment.center,
+        child: Text(tabTitle,
+            style: TextStyle(
+                fontSize: 14 * 1 / MediaQuery.of(context).textScaleFactor)),
+      ),
+    );
+  }
+}
