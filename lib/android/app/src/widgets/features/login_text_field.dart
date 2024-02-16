e883d8d78e7c9741ee9464f1127db19987552cb2
Fabio <fabioalta25@gmail.com>
2024-02-16 16:56:15 +0100

    UI changes

--- lib/android/app/src/widgets/features/login_text_field.dart
+++ lib/android/app/src/widgets/features/login_text_field.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:stream_me/android/app/src/utils/color_palette.dart';
 
-class LoginTextField extends StatelessWidget {
+class LoginTextField extends StatefulWidget {
   final TextEditingController? inputController; // user's input
   final bool obscureText; // obscured text for password in text field
   final String hintText; // hint text in text field
   final IconData prefixIcon;
 
-  //final Icon suffixIcon;
+  const LoginTextField(
+      {super.key,
+      required this.inputController,
+      required this.obscureText,
+      required this.hintText,
+      required this.prefixIcon});
 
-  const LoginTextField({
-    super.key,
-    required this.inputController,
-    required this.obscureText,
-    required this.hintText,
-    required this.prefixIcon,
-    /*required this.suffixIcon*/
-  });
+  @override
+  State<LoginTextField> createState() => _LoginTextFieldState();
+}
+
+class _LoginTextFieldState extends State<LoginTextField> {
+  bool _showPassword = true;
 
   @override
   Widget build(BuildContext context) {
@@ -24,46 +27,37 @@ class LoginTextField extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30.0),
       child: TextField(
-        controller: inputController, // get user's input
-        obscureText: obscureText,
+        controller: widget.inputController, // get user's input
+        obscureText: widget.obscureText ? _showPassword : false,
+        cursorColor: Colors.blueAccent,
         decoration: InputDecoration(
-            prefixIcon: Icon(prefixIcon, size: 28),
+            prefixIcon: Icon(widget.prefixIcon, size: 26),
             prefixIconColor: Colors.grey,
-            //TODO: suffixIcon
-            /*suffixIcon:
-                checkIconButton(suffixIcon, inputController, obscureText),*/
+            suffixIcon: widget.obscureText //show password icon
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _showPassword =
+                            !_showPassword; //switch between showing and not showing password
+                      });
+                    },
+                    icon: _showPassword
+                        ? const Icon(Icons.visibility,
+                            color: Colors.grey, size: 24)
+                        : const Icon(Icons.visibility_off,
+                            color: Colors.grey, size: 24))
+                : null,
             enabledBorder: OutlineInputBorder(
                 borderSide: const BorderSide(color: Colors.white),
-                borderRadius: BorderRadius.circular(20.0)),
+                borderRadius: BorderRadius.circular(30.0)),
             focusedBorder: OutlineInputBorder(
                 borderSide: const BorderSide(color: Colors.blueAccent),
-                borderRadius: BorderRadius.circular(20.0)),
+                borderRadius: BorderRadius.circular(30.0)),
             filled: true,
             fillColor: color.bodyTextColor,
-            hintText: hintText,
+            hintText: widget.hintText,
             hintStyle: const TextStyle(color: Colors.grey)),
       ),
     );
   }
-
-  /*
-  Widget checkIconButton(Icon icon, TextEditingController controller,
-      bool obscureText) {
-    obscureText = this.obscureText;
-    if (icon.toString() == const Icon(Icons.remove_red_eye).toString()) {
-      // check if suffixIcon is the needed one or not
-      print(obscureText);
-      return IconButton(onPressed: () {
-        obscureText = !obscureText;
-      },
-          icon: obscureText ? const Icon(
-              Icons.visibility) : const Icon(Icons.visibility_off));
-    } else {
-      return IconButton(
-          onPressed: null,
-          icon: icon,
-          disabledColor:
-          Colors.transparent); // disable other not needed suffixIcon
-    }
-   */
 }
