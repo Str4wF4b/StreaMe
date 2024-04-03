fb53dea6e54bc0931fd2f81a51db1b3fbd42bb82
Fabio <fabioalta25@gmail.com>
2024-04-03 23:03:21 +0200

    adding Functions-class with globally used functions

---
+++ lib/android/app/src/utils/functions.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:stream_me/android/app/src/auth/auth_main.dart';
+import 'package:stream_me/android/app/src/utils/color_palette.dart';
+
+class Functions {
+  final ColorPalette _color = ColorPalette();
+
+  Widget anonLoggedIn(BuildContext context, bool isTab) => Scaffold(
+        backgroundColor: _color.backgroundColor,
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          color: isTab ? _color.middleBackgroundColor : _color.backgroundColor,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15.0, top: 30.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "You are not logged in. ",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const Text("Login or register ",
+                    style: TextStyle(color: Colors.grey)),
+                GestureDetector(
+                  child: const Text(
+                    "here",
+                    style: TextStyle(color: Colors.blueAccent),
+                  ),
+                  onTap: () async => await FirebaseAuth.instance.signOut().then(
+                          (value) => Navigator.of(context).pushAndRemoveUntil(
+                          MaterialPageRoute(
+                              builder: (context) => const AuthPage()),
+                              (route) => false)),
+                ),
+                const Text(".", style: TextStyle(color: Colors.grey))
+              ],
+            ),
+          ),
+        ));
+}
