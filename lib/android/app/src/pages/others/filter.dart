f084fb844dd1dd98c5a3cdf03756434702679040
Fabio <fabioalta25@gmail.com>
2024-04-04 18:22:24 +0200

    code adjustments

--- lib/android/app/src/pages/others/filter.dart
+++ lib/android/app/src/pages/others/filter.dart
@@ -1,88 +1,63 @@
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:stream_me/android/app/src/data/actor_data.dart';
 import 'package:stream_me/android/app/src/pages/others/filter_results.dart';
 import 'package:stream_me/android/app/src/utils/color_palette.dart';
+import 'package:stream_me/android/app/src/utils/constants_and_values.dart';
 import '../../widgets/global/selection_button.dart';
 
 class FilterPage extends StatefulWidget {
-  FilterPage({Key? key}) : super(key: key);
-
-  final List<String> provider = [
-    "Apple TV",
-    "Apple TV+",
-    "Crunchyroll",
-    "Disney+",
-    "Hulu",
-    "Magenta TV",
-    "Netflix",
-    "Prime",
-    "Sky Go"
-  ];
-  String? value;
-  final List<String> type = ["Movie", "Series"];
-  final List<String> genre = [
-    "Action",
-    "Adventure",
-    "Animation",
-    "Comedy",
-    "Crime",
-    "Drama",
-    "Fantasy",
-    "Horror",
-    "Mystery",
-    "Science-Fiction",
-    "Thriller"
-  ];
+  const FilterPage({Key? key}) : super(key: key);
 
   @override
   State<FilterPage> createState() => _FilterPageState();
 }
 
 class _FilterPageState extends State<FilterPage> {
-  ColorPalette color = ColorPalette();
-  final List<String> actors = [];
-  final List<String> year = List<String>.generate(
-      130, (index) => "${index + 1895}"); //Years 1895 to 2024
+  // Utils:
+  final ColorPalette _color = ColorPalette();
+  final ConstantsAndValues _cav = ConstantsAndValues();
+
+  // Instances:
   final GlobalKey<FormFieldState> _keyPlatforms = GlobalKey();
   final GlobalKey<FormFieldState> _keyType = GlobalKey();
   final GlobalKey<FormFieldState> _keyGenre = GlobalKey();
   final GlobalKey<FormFieldState> _keyYear = GlobalKey();
   final GlobalKey<FormFieldState> _keyActor = GlobalKey();
 
+  // Local instances:
+  final List<String> _actors = [];
+  String? _value;
+
   @override
   void initState() {
     super.initState();
 
     for (var actor in allActors) {
-      actors.add(actor.displayName);
-    }
-    year.sort((b, a) => a.compareTo(b)); //sort years descending
-    actors.sort((a, b) => a.toLowerCase().compareTo(
-        b.toLowerCase())); //sort actors list before loading filter widget
+      _actors.add(actor.displayName);
+    } // fill actors list
+    _cav.years.sort((b, a) => a.compareTo(b)); // sort years in descending order
+    _actors.sort((a, b) => a.toLowerCase().compareTo(
+        b.toLowerCase())); // sort actors list in alphabetically ascending order
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: color.backgroundColor.withOpacity(0.93),
-      //Container transparent to show Dialog opacity background in SearchPage
-      width: MediaQuery.of(context).size.width,
-      //Filter screen with full window width
-      height: MediaQuery.of(context).size.height,
-      //Filter screen with full window height
+      color: _color.backgroundColor.withOpacity(0.93),
+      width: MediaQuery.of(context).size.width, // full window width
+      height: MediaQuery.of(context).size.height, // full window height
 
       child: Stack(children: [
         Padding(
           padding: const EdgeInsets.only(top: 33),
           child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Icon(Icons.tune, color: color.bodyTextColor, size: 30),
+            Icon(Icons.tune, color: _color.bodyTextColor, size: 30),
             const SizedBox(width: 10),
             Text(
               "Filters",
               style: TextStyle(
-                  color: color.bodyTextColor,
+                  color: _color.bodyTextColor,
                   fontWeight: FontWeight.bold,
                   fontSize: 30),
               textAlign: TextAlign.start,
@@ -91,72 +66,38 @@ class _FilterPageState extends State<FilterPage> {
         ),
         Padding(
           padding: const EdgeInsets.only(top: 28.0, bottom: 90.0),
-          //margin on top and bottom of the scrollable field
           child: SingleChildScrollView(
-            // scrolling up and down through filters
             child: Column(
               children: [
                 // Actual filters:
                 const SizedBox(height: 100.0),
-                //Streaming Platform, e.g Netflix, Prime:
-                makeFilter(widget.value, widget.provider, "Streaming Platforms",
+                // Streaming Platforms, e.g Netflix, Prime:
+                makeFilter(_value, _cav.platforms, "Streaming Platforms",
                     _keyPlatforms),
-                //Type, i.e. Movie or Series:
                 const SizedBox(height: 25.0),
-                makeFilter(widget.value, widget.type, "Type", _keyType),
-                //Genre, e.g. Action, Drama:
+
+                // Type, i.e. Movie or Series:
+                makeFilter(_value, _cav.types, "Type", _keyType),
                 const SizedBox(height: 25.0),
-                makeFilter(widget.value, widget.genre, "Genre", _keyGenre),
-                //Year, from 1895 to 2024
+
+                // Genre, e.g. Action, Drama:
+                makeFilter(_value, _cav.genres, "Genre", _keyGenre),
                 const SizedBox(height: 25.0),
-                makeFilter(widget.value, year, "Year", _keyYear),
-                /*Center(
-                  //Year Textfield
-                  child: Container(
-                    width: 330,
-                    padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-                    child: TextFormField(
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17,
-                          color: Colors.grey.shade100),
-                      keyboardType: TextInputType.number,
-                      inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.digitsOnly
-                      ],
-                      //allow only numbers in year-textfield
-                      decoration: InputDecoration(
-                        labelText: "Year",
-                        labelStyle: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey.shade400,
-                            fontSize: 18),
-                        hintText: "Search for year",
-                        contentPadding: EdgeInsets.fromLTRB(12.0, 22.0, 12.0, 22.0),
-                        isDense: true,
-                        hintStyle: TextStyle(
-                            color: Colors.grey.shade400,
-                            fontWeight: FontWeight.normal),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20.0)),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey.shade400),
-                            borderRadius: BorderRadius.circular(20.0)),
-                        filled: true,
-                        fillColor: Colors.black38,
-                      ),
-                    ),
-                  ),
-                ),*/
+
+                // Years, from 1895 to 2024:
+                makeFilter(_value, _cav.years, "Year", _keyYear),
                 const SizedBox(height: 25.0),
-                makeFilter(widget.value, actors, "Actor", _keyActor),
-                //search an Actor in the whole Database
+
+                // All actors:
+                makeFilter(_value, _actors, "Actor", _keyActor),
                 const SizedBox(height: 45),
+
                 Padding(
                   padding: const EdgeInsets.only(left: 60.0, right: 60.0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
+                      // Search Button:
                       SelectionButton(
                           onTap: () {
                             Navigator.push(
@@ -171,8 +112,10 @@ class _FilterPageState extends State<FilterPage> {
                           },
                           color: Colors.blueAccent,
                           label: "Search"),
+                      // Reset Button:
                       SelectionButton(
                           onTap: () {
+                            // Clear all Dropdown Form Field inputs:
                             _keyPlatforms.currentState?.reset();
                             _keyType.currentState?.reset();
                             _keyGenre.currentState?.reset();
@@ -200,7 +143,7 @@ class _FilterPageState extends State<FilterPage> {
                   onPressed: () => Navigator.pop(context),
                   icon: Icon(
                     Icons.close,
-                    color: color.middleBackgroundColor,
+                    color: _color.middleBackgroundColor,
                   )),
             ),
           ),
@@ -209,31 +152,35 @@ class _FilterPageState extends State<FilterPage> {
     );
   }
 
+  /// A function that generates a filter to choose between different options inside a DropdownButtonFormField
+  /// selectedValue: The currently selected value by a user
+  /// list: The list of all selectable options
+  /// label: The initial label of the Dropdown Button Form Field
+  /// keyController: The key of the currently used filter
   Center makeFilter(String? selectedValue, List<String> list, String label,
-      GlobalKey keyController) {
-    return Center(
+      GlobalKey keyController) => Center(
       child: Container(
         width: 330,
         padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-        //margin left and right edge
         child: DropdownButtonFormField2<String>(
           key: keyController,
           isExpanded: true,
           value: selectedValue,
-          //selected value stands in field
+          // selected value stands in field
           items: list.map(buildMenuItem).toList(),
-          //dropdown items
+          // dropdown items
           dropdownStyleData: DropdownStyleData(
               decoration: BoxDecoration(
-                  color: color.middleBackgroundColor,
+                  color: _color.middleBackgroundColor,
                   borderRadius: BorderRadius.circular(20.0)),
-              maxHeight: 202 * MediaQuery.of(context).textScaleFactor),
+              maxHeight: MediaQuery.textScalerOf(context).scale(202)),
           onChanged: (value) {
             setState(() {
-              selectedValue = value.toString();
+              selectedValue = value
+                  .toString(); // change current value to new selected value inside Dropdown menu
             });
           },
-          //Decoration of the label and border of the dropdown button
+          // Decoration of the Dropdown Button including label and border decorations:
           decoration: InputDecoration(
             labelText: label,
             contentPadding: const EdgeInsets.fromLTRB(20.0, 20.0, 12.0, 20.0),
@@ -255,12 +202,11 @@ class _FilterPageState extends State<FilterPage> {
         ),
       ),
     );
-  }
 
+  /// A function that builds the selected item from the Dropdown menu
+  /// item: The item selected by the user
   DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem(
       value: item,
-      //child: Padding(
-      //padding: const EdgeInsets.only(left: 5.0, right: 5.0),
       child: FittedBox(
         child: Text(
           item,
@@ -269,6 +215,5 @@ class _FilterPageState extends State<FilterPage> {
               fontSize: 17,
               color: Colors.grey.shade100),
         ),
-        //),
       ));
 }
