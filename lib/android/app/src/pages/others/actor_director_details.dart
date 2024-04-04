00a554ae7aa05d264181667a8ac2dc0b037e1858
Fabio <fabioalta25@gmail.com>
2024-04-04 15:56:02 +0200

    code adjustments

--- lib/android/app/src/pages/others/actor_director_details.dart
+++ lib/android/app/src/pages/others/actor_director_details.dart
@@ -23,23 +23,26 @@ class ActorDirectorDetailsPage extends StatefulWidget {
 
 class _ActorDirectorDetailsPageState extends State<ActorDirectorDetailsPage>
     with TickerProviderStateMixin {
+  // Utils:
   final ColorPalette _color = ColorPalette();
-  final ConstantsAndValues cons = ConstantsAndValues();
+  final ConstantsAndValues _cav = ConstantsAndValues();
 
-  final keyImage = GlobalKey();
-  Size? sizeImage;
-
+  // Instances:
   late final TabController _tabController =
       TabController(length: 3, vsync: this);
 
+  // Local instances:
+  final _keyImage = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
-    getSizeAndPosition();
+    readjustTabHeight();
     final sliverWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       backgroundColor: _color.backgroundColor,
       body: CustomRefreshIndicator(
+        // refresh page
         onRefresh: () {
           setState(() {});
           return Future.delayed(const Duration(milliseconds: 1200));
@@ -55,7 +58,6 @@ class _ActorDirectorDetailsPageState extends State<ActorDirectorDetailsPage>
           slivers: [
             SliverAppBar(
               backgroundColor: _color.backgroundColor,
-              //title: FittedBox(child: Text(widget.actorDirector.displayName)),
               elevation: 0.0,
               scrolledUnderElevation: 0.0,
               leading: IconButton(
@@ -66,7 +68,7 @@ class _ActorDirectorDetailsPageState extends State<ActorDirectorDetailsPage>
                   onPressed: () => Navigator.pop(context)),
               pinned: true,
               expandedHeight: MediaQuery.of(context).size.height * 0.375,
-              //298,
+              // Sliver effect of actor's and director's picture with their name:
               flexibleSpace: Container(
                 margin: EdgeInsets.only(
                     left: sliverWidth * 0.223, right: sliverWidth * 0.223),
@@ -89,14 +91,16 @@ class _ActorDirectorDetailsPageState extends State<ActorDirectorDetailsPage>
                       children: [
                         CachedNetworkImage(
                           imageUrl: widget.actorDirector.image,
+                          // the image of the actor / director
                           fit: BoxFit.fitHeight,
-                          key: keyImage,
+                          key: _keyImage,
                           placeholder: (context, url) =>
-                              cons.actorDirectorPlaceholder,
+                              _cav.actorDirectorPlaceholder,
                           errorWidget: (context, url, error) =>
-                              cons.imageErrorWidget,
+                              _cav.imageErrorWidget,
                         ),
                         Container(
+                          // faded text background effect
                           decoration: BoxDecoration(
                               gradient: LinearGradient(
                                   begin: FractionalOffset.topCenter,
@@ -107,19 +111,20 @@ class _ActorDirectorDetailsPageState extends State<ActorDirectorDetailsPage>
                                 Colors.black38,
                                 Colors.black38.withOpacity(0.0),
                               ],
-                                  stops: const [0.82,
-                                    0.88,
+                                  stops: const [
+                                0.82,
+                                0.88,
                                 0.92,
                                 0.98
                               ])),
                         )
                       ],
                     ),
-                    //titlePadding: const EdgeInsets.only(top: 0.0), //0.0 but necessary to put title on bottom of image
                   ),
                 ),
               ),
             ),
+            // The actor / director information below their picture:
             SliverToBoxAdapter(
               child: Center(
                 child: Padding(
@@ -129,34 +134,32 @@ class _ActorDirectorDetailsPageState extends State<ActorDirectorDetailsPage>
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       const SizedBox(height: 40),
+                      // Full Name:
                       actorDirectorInfo("Full Name:  ",
                           "${widget.actorDirector.firstName} ${widget.actorDirector.secondName}"),
                       const SizedBox(height: 20),
+                      // Age:
                       actorDirectorInfo("Age:  ",
                           "${getAge(widget.actorDirector.birthday)} (${widget.actorDirector.birthday})"),
                       const SizedBox(height: 20),
+                      // Birthplace:
                       actorDirectorInfo("Place of Birth:  ",
                           widget.actorDirector.placeOfBirth),
                       const SizedBox(height: 20),
+                      // Actor / Director Biography:
                       LayoutBuilder(builder: (context, constraints) {
-                        return /*checkForMaxLines(
-                              widget.stream.plot, context, constraints);*/
-                            ExpandText(widget.actorDirector.biography,
-                                style: TextStyle(
-                                    color: _color.bodyTextColor,
-                                    fontSize: 16 *
-                                        1 /
-                                        MediaQuery.of(context).textScaleFactor,
-                                    height: cons.textHeight),
-                                indicatorIcon: Icons.keyboard_arrow_down,
-                                indicatorIconColor: Colors.grey.shade400,
-                                indicatorPadding:
-                                    const EdgeInsets.only(bottom: 1.0),
-                                maxLines: /*MediaQuery.of(context).textScaleFactor == 1.1 ? 6 : 5*/
-                                    6,
-                                //TODO: !!
-                                expandIndicatorStyle:
-                                    ExpandIndicatorStyle.icon);
+                        return ExpandText(widget.actorDirector.biography,
+                            style: TextStyle(
+                                color: _color.bodyTextColor,
+                                fontSize:
+                                    MediaQuery.textScalerOf(context).scale(16),
+                                height: _cav.textHeight),
+                            indicatorIcon: Icons.keyboard_arrow_down,
+                            indicatorIconColor: Colors.grey.shade400,
+                            indicatorPadding:
+                                const EdgeInsets.only(bottom: 1.0),
+                            maxLines: 6,
+                            expandIndicatorStyle: ExpandIndicatorStyle.icon);
                       }),
                       const SizedBox(height: 20),
                       Column(
@@ -169,8 +172,7 @@ class _ActorDirectorDetailsPageState extends State<ActorDirectorDetailsPage>
                           const SizedBox(height: 20),
                           Align(
                             alignment: Alignment.centerLeft,
-                            //child: Padding(
-                            //padding: const EdgeInsets.only(bottom: 5.0),
+                            // TabBar with the actor's / director's Acting, Production and Direction career
                             child: TabBar(
                               physics: const ClampingScrollPhysics(),
                               dividerHeight: 0.0,
@@ -190,7 +192,6 @@ class _ActorDirectorDetailsPageState extends State<ActorDirectorDetailsPage>
                                 addTab("Direction", 2),
                               ],
                             ),
-                            //),
                           ),
                           SizedBox(
                             height: setTabHeight(
@@ -198,13 +199,16 @@ class _ActorDirectorDetailsPageState extends State<ActorDirectorDetailsPage>
                             child: AutoScaleTabBarView(
                                 controller: _tabController,
                                 children: [
+                                  // Content of "Acting"-Tab:
                                   ActorDirectorTab(
                                       actorDirector: widget.actorDirector,
                                       tabContent: widget.actorDirector.acting),
+                                  // Content of "Production"-Tab:
                                   ActorDirectorTab(
                                       actorDirector: widget.actorDirector,
                                       tabContent:
                                           widget.actorDirector.production),
+                                  // Content of "Direction"-Tab:
                                   ActorDirectorTab(
                                       actorDirector: widget.actorDirector,
                                       tabContent:
@@ -224,18 +228,14 @@ class _ActorDirectorDetailsPageState extends State<ActorDirectorDetailsPage>
     );
   }
 
-  /// A function that determines the size and position of a specific Widget
-  void getSizeAndPosition() =>
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        final RenderBox boxImage =
-            keyImage.currentContext!.findRenderObject() as RenderBox;
-
-        setState(() {
-          sizeImage = boxImage.size;
-        });
+  /// A function that automatically sets the exactly needed height of each Tab
+  void readjustTabHeight() => WidgetsBinding.instance.addPostFrameCallback((_) {
+        setState(() {});
       });
 
-  /// A function that makes makes a simple row of Actor or Director information
+  /// A function that generates a row of Actor or Director information
+  /// label: The constant label of the information
+  /// input: The different information of each actor or director
   AutoSizeText actorDirectorInfo(String label, String input) =>
       AutoSizeText.rich(
         TextSpan(
@@ -257,7 +257,9 @@ class _ActorDirectorDetailsPageState extends State<ActorDirectorDetailsPage>
         textAlign: TextAlign.center,
       );
 
-  /// a function that adds the look of a tab
+  /// A function that adds the 3 tabs for each Actor / Director
+  /// tabTitle: The title of the tab (Acting, Production or Direction)
+  /// tabIndex: The index of the tab (0, 1 or 2)
   Widget addTab(String tabTitle, int tabIndex) {
     return Tab(
         child: StreaMeTab(
@@ -268,44 +270,82 @@ class _ActorDirectorDetailsPageState extends State<ActorDirectorDetailsPage>
     ));
   }
 
-  /// a function that
+  /// A function that calculates the current age for a person
+  /// actorAge: The birth date (and death date) of a person
   int getAge(String actorAge) {
     int age = 0;
 
-    String actorDay = actorAge.substring(0, 2);
-    String actorMonth = actorAge.substring(3, 5);
-    String actorYear = actorAge.substring(6);
-    String actorBirthday = '$actorDay-$actorMonth-$actorYear';
-    DateTime actorBirthdayDateTime =
-        DateFormat('dd-MM-yyyy').parse(actorBirthday);
+    String actorDay = actorAge.substring(0, 2); // index 0 and 1: day (2 is ".")
+    String actorMonth =
+        actorAge.substring(3, 5); // index 3 and 4: month (5 is ".")
+    String actorYear = actorAge.substring(6); // index 6 and rest: year
+    String actorBirthday =
+        '$actorDay-$actorMonth-$actorYear'; // transform day, month and year into valid date format String
+    DateTime actorBirthdayDateTime = DateFormat('dd-MM-yyyy')
+        .parse(actorBirthday); // generate date format DateFormat
 
-    if (actorBirthdayDateTime.month < DateTime.now().month ||
-        (actorBirthdayDateTime.month == DateTime.now().month &&
-            actorBirthdayDateTime.month <= DateTime.now().month)) {
-      age = DateTime.now().year - actorBirthdayDateTime.year;
+    // Calculate age:
+    if (!actorAge.contains("†")) {
+      // if person is not dead
+      if (actorBirthdayDateTime.month <
+              DateTime.now()
+                  .month || // if month is lower than current month or ...
+          (actorBirthdayDateTime.month == DateTime.now().month &&
+              actorBirthdayDateTime.day <= DateTime.now().day)) {
+        // ... if months are equal and day is lower or equal than current day
+        age = DateTime.now().year -
+            actorBirthdayDateTime.year; // calculate normally
+      } else {
+        age = DateTime.now().year -
+            actorBirthdayDateTime.year -
+            1; // calculate additional -1 because birthday hasn't been yet
+      }
     } else {
-      age = DateTime.now().year - actorBirthdayDateTime.year - 1;
-    }
+      String deathDay =
+          actorAge.substring(14, 16); // index 14 and 15: death day (16 is ".")
+      String deathMonth = actorAge.substring(
+          17, 19); // index 17 and 19: death month (20 is ".")
+      String deathYear = actorAge.substring(20); // index 20 and rest: year
+      String actorDeathDate =
+          '$deathDay-$deathMonth-$deathYear'; // transform death day, death month and death year into valid date format String
+      DateTime actorDeathDateTime = DateFormat('dd-MM-yyyy')
+          .parse(actorDeathDate); // generate date format DateFormat
 
-    //TODO: Check for age for dead people
+      // Calculate age till death:
+      if (actorDeathDateTime.month <
+              actorBirthdayDateTime
+                  .month || // if month is lower than current month or ...
+          (actorDeathDateTime.month == actorBirthdayDateTime.month &&
+              actorDeathDateTime.day <= actorBirthdayDateTime.day)) {
+        // ... if months are equal and day is lower or equal than current day
+        age = actorDeathDateTime.year -
+            actorBirthdayDateTime.year; // calculate normally
+      } else {
+        age = actorDeathDateTime.year -
+            actorBirthdayDateTime.year -
+            1; // calculate additional -1 because birthday hasn't been reached
+      }
+    }
 
     return age;
   }
 
-  /// a function that individually sets the height of a tab by calling the movies and series list for each tab
-  double setTabHeight(Actor actorDirector, int tab) {
+  /// A function that sets the height of a tab by fetching the movies and series list for each tab (Acting, Production and Direction)
+  /// actorDirector: The current actor or director
+  /// tab: The index of the tab (0, 1 or 2)
+  double setTabHeight(Actor actorDirector, int tabIndex) {
     double tabHeight = 0;
     List? movies;
     List? series;
-    if (tab == 0) {
+    if (tabIndex == 0) {
       movies = actorDirector.acting['Movies'];
       series = actorDirector.acting['Series'];
     }
-    if (tab == 1) {
+    if (tabIndex == 1) {
       movies = actorDirector.production['Movies'];
       series = actorDirector.production['Series'];
     }
-    if (tab == 2) {
+    if (tabIndex == 2) {
       movies = actorDirector.directing['Movies'];
       series = actorDirector.directing['Series'];
     }
@@ -313,18 +353,18 @@ class _ActorDirectorDetailsPageState extends State<ActorDirectorDetailsPage>
     return tabHeight;
   }
 
-  /// a function that decides whether the height of a tab is 530, 350 or 160 depending on empty movies and series lists
+  /// A function that decides whether the height of a tab is 530, 350 or 160 depending on the content of the lists
+  /// movies: The list of movies a person has acted in, produced or directed
+  /// series: The list of series a person has acted in, produced or directed
   double getTabHeight(List movies, List series) {
     if (movies.isNotEmpty && series.isNotEmpty) {
-      //max height if movies and series list has input
-      return 530;
+      return 530; // max height if movies and series lists have input
     } else if (movies.isNotEmpty &&
-            series.isEmpty || //mid height if one of the lists is empty
+            series.isEmpty ||
         series.isNotEmpty && movies.isEmpty) {
-      return 350;
+      return 350; // mid height if one of the lists is empty
     } else {
-      //min height if both lists are empty
-      return 160;
+      return 160; // min height if both lists are empty
     }
   }
 }
